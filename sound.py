ce2b4a1386fa5969e6dd26c23eb95578b452c5d7
HeloHi <helomegido@gmail.com>
2023-12-24 15:13:56 +0400

    More sounds

--- sound.py
+++ sound.py
@@ -5,3 +5,7 @@ class Sound:
     @staticmethod
     def play_boing():
         return pygame.mixer.Sound('./assets/boing.wav').play()
+
+    @staticmethod
+    def play_point_gain():
+        return pygame.mixer.Sound('./assets/point.wav').play()
